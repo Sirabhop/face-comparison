96e23990a36a5a4fc789f39eb58cbc0334381a07
Jay Sirabhop <39372206+Sirabhop@users.noreply.github.com>
2024-03-05 18:26:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,28 @@ cut_off = st.slider('Cut-Off Confidence', min_value=0.0, max_value=1.0, value=0.
 
 base_face = st.file_uploader("Your based face", type=['jpg','png','jpeg'], accept_multiple_files=False)
 st.image(base_face)
-if base_face_id:
+if base_face:
     uploaded_face = st.file_uploader("Upload Your Face", type=['jpg','png','jpeg'], accept_multiple_files=False)
-
-    
-    # base_face = frc.load_image_file(f'/Users/sirabhopsaengumyoun/Desktop/My Project/face_comparision/face_database/{str(base_face_id)}.jpg')
-    # base_face = frc.load_image_file(f'face_database/{str(base_face_id)}.jpg')
-
-    encoded_base_face = frc.face_encodings(base_face)
     if uploaded_face:
-        face = frc.load_image_file(uploaded_face)
-        encoded_comparing_face = frc.face_encodings(face)
-        distance = frc.face_distance(encoded_comparing_face, encoded_base_face[0])
-
-        if distance.size > 0:
-
-            accuracy = 1 - distance[0]
-
-            if accuracy >= cut_off:
-                st.write(f':green[Your verification has been APPROVED with {round(accuracy*100, 2)} confidence.]')
+        st.image(base_face)
+        # base_face = frc.load_image_file(f'/Users/sirabhopsaengumyoun/Desktop/My Project/face_comparision/face_database/{str(base_face_id)}.jpg')
+        # base_face = frc.load_image_file(f'face_database/{str(base_face_id)}.jpg')
+    
+        encoded_base_face = frc.face_encodings(base_face)
+        if uploaded_face:
+            face = frc.load_image_file(uploaded_face)
+            encoded_comparing_face = frc.face_encodings(face)
+            distance = frc.face_distance(encoded_comparing_face, encoded_base_face[0])
+    
+            if distance.size > 0:
+    
+                accuracy = 1 - distance[0]
+    
+                if accuracy >= cut_off:
+                    st.write(f':green[Your verification has been APPROVED with {round(accuracy*100, 2)} confidence.]')
+                else:
+                    st.write(f':red[Your verification has been REJECTED with {round(accuracy*100, 2)} confidence.]', )
+                    st.write(':red[Sorry to see your dissapointment, you may want to re-upload.]')
+    
             else:
-                st.write(f':red[Your verification has been REJECTED with {round(accuracy*100, 2)} confidence.]', )
-                st.write(':red[Sorry to see your dissapointment, you may want to re-upload.]')
-
-        else:
-            st.write(':red[Please upload a clear face.]')
+                st.write(':red[Please upload a clear face.]')
