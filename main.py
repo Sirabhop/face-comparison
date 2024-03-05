eac09e6e0c125853f04767a455be6485b27f0c65
Jay Sirabhop <39372206+Sirabhop@users.noreply.github.com>
2024-03-05 18:37:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ st.title("Face Comparison Experiment")
 st.write('For riders facial verification')
 cut_off = st.slider('Cut-Off Confidence', min_value=0.0, max_value=1.0, value=0.8)
 
+# Get input base face
 base_face = st.file_uploader("Your based face", type=['jpg','png','jpeg'], accept_multiple_files=False)
 if base_face:
     st.image(base_face)
     uploaded_face = st.file_uploader("Upload Your Face", type=['jpg','png','jpeg'], accept_multiple_files=False)
+
+    # Get input uploaded_face
     if uploaded_face:
         st.image(uploaded_face)
         # base_face = frc.load_image_file(f'/Users/sirabhopsaengumyoun/Desktop/My Project/face_comparision/face_database/{str(base_face_id)}.jpg')
@@ -16,6 +19,7 @@ if base_face:
         encoded_base_face = frc.face_encodings(base_face)
         face = frc.load_image_file(uploaded_face)
         encoded_comparing_face = frc.face_encodings(face)
+        
         distance = frc.face_distance(encoded_comparing_face, encoded_base_face[0])
 
         if distance.size > 0:
