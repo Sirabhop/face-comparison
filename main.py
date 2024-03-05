1f3328c9f6612362fcdab52b4715933e8a6ba4c3
Jay Sirabhop <39372206+Sirabhop@users.noreply.github.com>
2024-03-05 18:36:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,25 +10,23 @@ if base_face:
     st.image(base_face)
     uploaded_face = st.file_uploader("Upload Your Face", type=['jpg','png','jpeg'], accept_multiple_files=False)
     if uploaded_face:
-        st.image(base_face)
+        st.image(uploaded_face)
         # base_face = frc.load_image_file(f'/Users/sirabhopsaengumyoun/Desktop/My Project/face_comparision/face_database/{str(base_face_id)}.jpg')
         # base_face = frc.load_image_file(f'face_database/{str(base_face_id)}.jpg')
-    
         encoded_base_face = frc.face_encodings(base_face)
-        if uploaded_face:
-            face = frc.load_image_file(uploaded_face)
-            encoded_comparing_face = frc.face_encodings(face)
-            distance = frc.face_distance(encoded_comparing_face, encoded_base_face[0])
-    
-            if distance.size > 0:
-    
-                accuracy = 1 - distance[0]
-    
-                if accuracy >= cut_off:
-                    st.write(f':green[Your verification has been APPROVED with {round(accuracy*100, 2)} confidence.]')
-                else:
-                    st.write(f':red[Your verification has been REJECTED with {round(accuracy*100, 2)} confidence.]', )
-                    st.write(':red[Sorry to see your dissapointment, you may want to re-upload.]')
-    
+        face = frc.load_image_file(uploaded_face)
+        encoded_comparing_face = frc.face_encodings(face)
+        distance = frc.face_distance(encoded_comparing_face, encoded_base_face[0])
+
+        if distance.size > 0:
+
+            accuracy = 1 - distance[0]
+
+            if accuracy >= cut_off:
+                st.write(f':green[Your verification has been APPROVED with {round(accuracy*100, 2)} confidence.]')
             else:
-                st.write(':red[Please upload a clear face.]')
+                st.write(f':red[Your verification has been REJECTED with {round(accuracy*100, 2)} confidence.]', )
+                st.write(':red[Sorry to see your dissapointment, you may want to re-upload.]')
+
+        else:
+            st.write(':red[Please upload a clear face.]')
